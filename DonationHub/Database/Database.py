4be70bffd81af3f103675b5815b4dc326a75920a
Anshika <pg73695@umbc.edu>
2022-04-18 15:53:18 -0400

    Database Complete

--- DonationHub/Database/Database.py
+++ DonationHub/Database/Database.py
@@ -12,34 +12,34 @@ cur.execute("DROP TABLE IF EXISTS charities")
  
 # Creating the tables
 cur.execute('''CREATE TABLE states(
-        ID INT PRIMARY KEY,
+        STATEID INT PRIMARY KEY,
    	STATENAME TEXT,
 	ABBREVIATION TEXT,
         STATEPOP INT);''')
 
 cur.execute('''CREATE TABLE counties(
         STATEID INT,
-        ID INT PRIMARY KEY,
+        COUNTYID INT PRIMARY KEY,
    	COUNTYNAME TEXT,
         COUNTYPOP INT,
-        FOREIGN KEY(STATEID) REFERENCES states(ID));''')
+        FOREIGN KEY(STATEID) REFERENCES states(STATEID));''')
 
 cur.execute('''CREATE TABLE poverty(
         COUNTYID INT PRIMARY KEY,
         POVERTY_ESTIMATE INT,
-        FOREIGN KEY(COUNTYID) REFERENCES counties(ID));''')
+        FOREIGN KEY(COUNTYID) REFERENCES counties(COUNTYID));''')
 
 cur.execute('''CREATE TABLE unemployment(
         COUNTYID INT PRIMARY KEY,
         UNEMPLOYED_PEOPLE INT,
-        UNEMPLOYED_RATE INT,
+        UNEMPLOYMENT_RATE INT,
         FOREIGN KEY(COUNTYID) REFERENCES counties(ID));''')
 
 cur.execute('''CREATE TABLE charities(
         STATEID INT,
-        ID INT PRIMARY KEY,
+        ORGID INT PRIMARY KEY,
         ORGANIZATION_NAME TEXT,
-        FOREIGN KEY(STATEID) REFERENCES states(ID));''')
+        FOREIGN KEY(STATEID) REFERENCES states(STATEID));''')
 
 #Inserting data into the tables
 a_file = open("StatePopulations.csv")
@@ -49,15 +49,15 @@ d_file = open("unemployment.csv")
 e_file = open("charities.csv")
 
 rows = csv.reader(a_file)
-cur.executemany("INSERT INTO states (ID,STATENAME,ABBREVIATION,STATEPOP) VALUES (?,?,?,?);", rows)
+cur.executemany("INSERT INTO states (STATEID,STATENAME,ABBREVIATION,STATEPOP) VALUES (?,?,?,?);", rows)
 rows = csv.reader(b_file)
-cur.executemany("INSERT INTO counties (STATEID,ID,COUNTYNAME,COUNTYPOP) VALUES (?,?,?,?);", rows)
+cur.executemany("INSERT INTO counties (STATEID,COUNTYID,COUNTYNAME,COUNTYPOP) VALUES (?,?,?,?);", rows)
 rows = csv.reader(c_file)
 cur.executemany("INSERT OR IGNORE INTO poverty (COUNTYID,POVERTY_ESTIMATE) VALUES (?,?);", rows)
 rows = csv.reader(d_file)
 cur.executemany("INSERT OR IGNORE INTO unemployment (COUNTYID,UNEMPLOYED_PEOPLE,UNEMPLOYMENT_RATE) VALUES (?,?,?);", rows)
 rows = csv.reader(e_file)
-cur.executemany("INSERT OR IGNORE INTO charities (STATEID,ID,ORGANIZATION_NAME) VALUES (?,?,?);", rows)
+cur.executemany("INSERT OR IGNORE INTO charities (STATEID,ORGID,ORGANIZATION_NAME) VALUES (?,?,?);", rows)
 
 con.commit()
 con.close()
