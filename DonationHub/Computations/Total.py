cae13298575ee8dffff3881f12e9ef895e3d48d7
Anshika <pg73695@umbc.edu>
2022-04-24 10:29:34 -0400

    Computation Finish

--- DonationHub/Computations/Total.py
+++ DonationHub/Computations/Total.py
@@ -12,6 +12,20 @@ def Total(file,colIndex):
                 s = s + int(x)
         return s
 
+def Average(file,colIndex):
+        s = Total(file,colIndex)
+        dataset = pd.read_csv(file)
+        df = pd.DataFrame(dataset)
+        avg = s/len(list(df[df.columns[colIndex]]))
+        return avg
+
+def Count(file,colIndex):
+        dataset = pd.read_csv(file)
+        df = pd.DataFrame(dataset)
+        column = list(df[df.columns[colIndex]])
+        c = len(column)
+        return c
+
 def main():
         #Just change file to any dataset csv file and just enter the column number you want to calculater --- Oh remember that column numbers start at 0 so 3rd column is actually 2 not 3
 
@@ -26,15 +40,18 @@ def main():
         #colnum = 3
 
         #Example of poverty estimate
-        #file = "poverty.csv"
-        #colnum = 1
+        file = "poverty.csv"
+        colnum = 1
 
         #Example of Unemployed People
         #file = "unemployment.csv"
         #colnum = 1
         
         total = Total(file,colnum)
+        avg = Average(file,colnum)
         print("SUM: " + str(total))
+        print("Average: " + str(avg))
+
 main()
 
 
