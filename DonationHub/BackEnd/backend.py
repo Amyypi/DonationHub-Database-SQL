af9aa0e42f2437043308576d3ecbdbf33f274ef7
Anshika <pg73695@umbc.edu>
2022-05-06 19:24:30 -0400

    Backend Complete

--- DonationHub/BackEnd/backend.py
+++ DonationHub/BackEnd/backend.py
@@ -49,21 +49,19 @@ def getOnlyUnemploymentTable(stateLst,countyLst):
         records = cur.fetchall()
         return records
 
-def getOnlyCharitiesTable(stateLst,countyLst):
+def getOnlyCharitiesTable(stateLst):
         query1 = "create temp table tb(STATEID INT,STATENAME TEXT,ABBREVIATION TEXT,STATEPOP INT,ORGID INT,ORGANIZATION_NAME TEXT)"
         query2 = "insert into tb select s.*, c.ORGID, c.ORGANIZATION_NAME from states as s natural join charities as c"
         cur.execute(query1)
         cur.execute(query2)
-        query3 = f"create temp table tb1 as SELECT * FROM tb WHERE tb.STATENAME in ({','.join(['?']*len(stateLst))})"
-        cur.execute(query3, stateLst)
-        query4 = f"SELECT * FROM tb1 WHERE tb1.COUNTYNAME in ({','.join(['?']*len(countyLst))})"
-        cur.execute(query4, countyLst)
+        query3 = f"SELECT tb.* FROM tb WHERE tb.STATENAME in ({','.join(['?']*len(stateLst))})"
+        cur.execute(query3,stateLst)
         records = cur.fetchall()
         return records
 
 def getDefaultTable(stateLst,countyLst):
         query1 = "create temp table tb(STATEID INT,STATENAME TEXT,ABBREVIATION TEXT,STATEPOP INT,COUNTYID INT,COUNTYNAME TEXT,COUNTYPOP INT, POVERTY_ESTIMATE INT,UNEMPLOYED_PEOPLE INT, UNEMPLOYMENT_RATE INT)"
-        query2 = "insert into tb select s.*, c.COUNTYID, c.COUNTYNAME, c.COUNTYPOP, p.POVERTY_ESTIMATE, u.UNEMPLOYED_PEOPLE, u.UNEMPLOYMENT_RATE from states as s natural join counties as c natural join natural join poverty as p unemployment as u"
+        query2 = "insert into tb select s.*, c.COUNTYID, c.COUNTYNAME, c.COUNTYPOP, p.POVERTY_ESTIMATE, u.UNEMPLOYED_PEOPLE, u.UNEMPLOYMENT_RATE from states as s natural join counties as c natural join poverty as p natural join unemployment as u"
         cur.execute(query1)
         cur.execute(query2)
         query3 = f"create temp table tb1 as SELECT * FROM tb WHERE tb.STATENAME in ({','.join(['?']*len(stateLst))})"
@@ -133,10 +131,7 @@ def getTotalCharities(records):
 
 def main():
         states = ['Maryland','Alaska']
-        counties = ["Howard","Baltimore"]
-        records = getOnlyUnemploymentTable(states,counties)
-        a = getTotalUnEmployedPeople(records)
-        print(a)
+        records = getOnlyCharitiesTable(states)
         df = pd.DataFrame(records)
         df.to_csv("sol.csv")
         con.commit()
