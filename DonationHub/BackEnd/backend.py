cab12ad0374b54bf219f8189f8433bda196cda67
Amy Phan <49791121+Amyypi@users.noreply.github.com>
2022-05-05 21:41:49 -0400

    Unemployment dashboard base done

--- DonationHub/BackEnd/backend.py
+++ DonationHub/BackEnd/backend.py
@@ -16,6 +16,8 @@ cur = con.cursor()
 #class table_queries():
 # View and Retrieve Data
 def getOnlyStateAndCountyTable(stateLst,countyLst):
+        con = sqlite3.connect("data.db", check_same_thread=False )
+        cur = con.cursor()
         query1 = "create temp table tb(STATEID INT, STATENAME TEXT, ABBREVIATION TEXT, STATEPOP INT,COUNTYID INT, COUNTYNAME TEXT, COUNTYPOP INT)"
         query2 = "insert into tb select s.*, c.COUNTYID, c.COUNTYNAME, c.COUNTYPOP from states as s natural join counties as c"
         cur.execute(query1)
@@ -25,10 +27,13 @@ def getOnlyStateAndCountyTable(stateLst,countyLst):
         query4 = f"SELECT * FROM tb1 WHERE tb1.COUNTYNAME in ({','.join(['?']*len(countyLst))})"
         cur.execute(query4, countyLst)
         records = cur.fetchall()
+        con.commit()
+        con.close()
         return records
 
 def getOnlyPovertyTable(stateLst,countyLst):
-        
+        con = sqlite3.connect("data.db", check_same_thread=False )
+        cur = con.cursor()
         query1 = "create temp table tb(STATEID INT,STATENAME TEXT,ABBREVIATION TEXT,STATEPOP INT,COUNTYID INT,COUNTYNAME TEXT,COUNTYPOP INT,POVERTY_ESTIMATE INT)"
         query2 = "insert into tb select s.*, c.COUNTYID, c.COUNTYNAME, c.COUNTYPOP, p.POVERTY_ESTIMATE from states as s natural join counties as c natural join poverty as p"
         cur.execute(query1)
@@ -38,6 +43,8 @@ def getOnlyPovertyTable(stateLst,countyLst):
         query4 = f"SELECT * FROM tb1 WHERE tb1.COUNTYNAME in ({','.join(['?']*len(countyLst))})"
         cur.execute(query4, countyLst)
         records = cur.fetchall()
+        con.commit()
+        con.close()
         return records
 
 def getOnlyUnemploymentTable(stateLst,countyLst):
@@ -57,6 +64,8 @@ def getOnlyUnemploymentTable(stateLst,countyLst):
         return records
 
 def getOnlyCharitiesTable(stateLst,countyLst):
+        con = sqlite3.connect("data.db", check_same_thread=False )
+        cur = con.cursor()
         query1 = "create temp table tb(STATEID INT,STATENAME TEXT,ABBREVIATION TEXT,STATEPOP INT,ORGID INT,ORGANIZATION_NAME TEXT)"
         query2 = "insert into tb select s.*, c.ORGID, c.ORGANIZATION_NAME from states as s natural join charities as c"
         cur.execute(query1)
@@ -66,9 +75,13 @@ def getOnlyCharitiesTable(stateLst,countyLst):
         query4 = f"SELECT * FROM tb1 WHERE tb1.COUNTYNAME in ({','.join(['?']*len(countyLst))})"
         cur.execute(query4, countyLst)
         records = cur.fetchall()
+        con.commit()
+        con.close()
         return records
 
 def getDefaultTable(stateLst,countyLst):
+        con = sqlite3.connect("data.db", check_same_thread=False )
+        cur = con.cursor()
         query1 = "create temp table tb(STATEID INT,STATENAME TEXT,ABBREVIATION TEXT,STATEPOP INT,COUNTYID INT,COUNTYNAME TEXT,COUNTYPOP INT, POVERTY_ESTIMATE INT,UNEMPLOYED_PEOPLE INT, UNEMPLOYMENT_RATE INT)"
         query2 = "insert into tb select s.*, c.COUNTYID, c.COUNTYNAME, c.COUNTYPOP, p.POVERTY_ESTIMATE, u.UNEMPLOYED_PEOPLE, u.UNEMPLOYMENT_RATE from states as s natural join counties as c natural join natural join poverty as p unemployment as u"
         cur.execute(query1)
@@ -78,9 +91,13 @@ def getDefaultTable(stateLst,countyLst):
         query4 = f"SELECT * FROM tb1 WHERE tb1.COUNTYNAME in ({','.join(['?']*len(countyLst))})"
         cur.execute(query4, countyLst)
         records = cur.fetchall()
+        con.commit()
+        con.close()
         return records
 
 def getCountiesList(stateLst):
+        con = sqlite3.connect("data.db", check_same_thread=False )
+        cur = con.cursor()
         query1 = "create temp table tb (STATEID INT, STATENAME TEXT, ABBREVIATION TEXT, STATEPOP INT,COUNTYID INT, COUNTYNAME TEXT, COUNTYPOP INT)"
         query2 = "insert into tb select s.*, c.COUNTYID, c.COUNTYNAME, c.COUNTYPOP from states as s natural join counties as c"
         cur.execute(query1)
@@ -88,6 +105,8 @@ def getCountiesList(stateLst):
         query3 = f"SELECT tb.COUNTYNAME FROM tb WHERE tb.STATENAME in ({','.join(['?']*len(stateLst))})"
         cur.execute(query3,stateLst)
         records = cur.fetchall()
+        con.commit()
+        con.close()
         return records
 
 # Computations
@@ -106,6 +125,7 @@ def Total(data):
 def Count(data):
         return len(data)
 
+# Population Computation
 def getCountiesTotalPopulation(records):
         data = getData(records,countyPop)
         return Total(data)
@@ -114,6 +134,7 @@ def getCountiesAveragePopulation(records):
         data = getData(records,countyPop)
         return Average(data)
 
+# Poverty Computation
 def getTotalPovertyEstimates(records):
         data = getData(records,povertyEstimate)
         return Total(data)
@@ -122,6 +143,7 @@ def getAveragePovertyEstimates(records):
         data = getData(records,povertyEstimate)
         return Average(data)
 
+# Unemployment Computation
 def getTotalUnEmployedPeople(records):
         data = getData(records,unemployedPeople)
         return Total(data)
@@ -134,12 +156,10 @@ def getAverageUnEmploymentRate(records):
         data = getData(records,unemploymentRate)
         return Average(data)
 
+# Charities Computation
 def getTotalCharities(records):
         data = getData(records,unemploymentRate)
         return Count(data)
-
-def test_count():
-        return 1
 
 #
 #def main():
--- DonationHub/routes.py
+++ DonationHub/routes.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, url_for, flash, redirect, request, jsonify, json
 from sqlalchemy import func, select
-from sqlalchemy.orm import Bundle
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from models import charities, counties, poverty, states, unemployment, Form
 from BackEnd.backend import *
 from flask_wtf import FlaskForm
-from wtforms import SelectField
+from wtforms import SelectField, SelectMultipleField, Form
+from wtforms.fields import SelectMultipleField, SubmitField
+from statistics import mean
+
 import sqlite3
 import pandas as pd
-from statistics import mean
 import requests
 
 #initialize the flask app
@@ -23,26 +24,47 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
 #initialize the database
 db = SQLAlchemy(app)
-
 con = sqlite3.connect("data.db", check_same_thread=False )
 cur = con.cursor()
 
+# For state dropdown
+choices = []
+class Form(FlaskForm):
+    global choices
+    state = SelectField('state', choices=[])
+    county = SelectField('county', choices=[])
+    select_multiple_field = SelectMultipleField(choices=choices)
+    position = SelectMultipleField(u'Choose position:')
+    submit = SubmitField()
+
+
+
+########################################
+# Unemployment 
+########################################
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # Headings for the table
     headings = ("State FIPS","State Name","Abbreviation","State Population", "County FIPS","County Name","County Population", "Unemployed People","Unemployed Rate")
-
     # Make dropdown for state options
     form = Form()
     form.state.choices = [(state.STATEID, state.STATENAME) for state in states.query.all()]
-  
-    # put states in a list
+
+    # SELECT 
+    if request.method == 'POST':
+        state_l = states.query.filter_by(STATEID=form.state.data).first()
+        stateArray = []
+        stateObj = state_l.STATENAME
+        stateArray.append(stateObj)
+        return redirect(url_for('unemployment_county', state_l=stateArray))
+
+     # put states in a list
     state_na = db.session.query(states.STATENAME).all()
     stateArray = []
     for state_info in state_na:
         stateObj = state_info.STATENAME
         stateArray.append(stateObj)
-    print(stateArray)
+    #print(stateArray)
 
     # put counties in a list
     count_na = db.session.query(counties.COUNTYNAME).all()
@@ -50,54 +72,56 @@ def index():
     for county_info in count_na:
         countyObj = county_info.COUNTYNAME
         countyArray.append(countyObj)
+    choices = countyArray
+
+    form_data = form.select_multiple_field.data
+    print(form_data)
 
     # Make table
-    result = getOnlyUnemploymentTable(stateArray,countyArray)
+    result = getOnlyUnemploymentTable(stateArray,choices)
+    tot_unemp_people = getTotalUnEmployedPeople(result)
+    avg_unemp_people = getAverageUnEmployedPeople(result)
+    avg_unemp_rate = getAverageUnEmploymentRate(result)
+    return render_template('unemployment_table_state.html', form=form, result=result, headings=headings, tot_unemp_people=tot_unemp_people, avg_unemp_people=avg_unemp_people, avg_unemp_rate=avg_unemp_rate)
 
-    # SELECT 
-    if request.method == 'POST':
-        county = counties.query.filter_by(STATEID=form.county.data).first()
-        state = states.query.filter_by(STATEID=form.state.data).first()
+@app.route('/unemployment_county', methods=['GET', 'POST'])
+def unemployment_county():
+    state_l = request.args['state_l']
+    headings = ("State FIPS","State Name","Abbreviation","State Population", "County FIPS","County Name","County Population", "Unemployed People","Unemployed Rate")
+    state = states.query.filter(states.STATENAME==state_l).first()
+    county_l = counties.query.filter(counties.STATEID==state.STATEID).all()
 
-        # put states in a list
-        stateArray = []
-        stateObj = state.STATENAME
-        stateArray.append(stateObj)
+    stateArray = []
+    statename = state.STATENAME
+    stateArray.append(statename)
+    print("county in post", county_l)
 
-        # put counties in a list
-        countyArray = []
-        countyObj = county.COUNTYNAME
+    countyArray = []
+    for county_info in county_l:
+        countyObj = county_info.COUNTYNAME
         countyArray.append(countyObj)
 
-        # Make table
-        result = getOnlyUnemploymentTable(stateArray,countyArray)
+    if request.method == 'POST':
+        # Retrieve county selection from dropdown
+        county_list = request.form.getlist('c_list')
 
+        # Make table
+        result = getOnlyUnemploymentTable(stateArray,county_list)
         # Data computations
         tot_unemp_people = getTotalUnEmployedPeople(result)
         avg_unemp_people = getAverageUnEmployedPeople(result)
         avg_unemp_rate = getAverageUnEmploymentRate(result)
 
-        state_n = state.STATENAME
-        county_n = county.COUNTYNAME
-        return render_template('unemployment_table.html', form=form, result=result, headings=headings, tot_unemp_people=tot_unemp_people, avg_unemp_people=avg_unemp_people, avg_unemp_rate=avg_unemp_rate)
-    
+        return render_template('unemployment_table_county.html', county_list=county_l, result=result, headings=headings, tot_unemp_people=tot_unemp_people, avg_unemp_people=avg_unemp_people, avg_unemp_rate=avg_unemp_rate)
+
+    # Make table
+    result = getOnlyUnemploymentTable(stateArray,countyArray)
+    # Data computations
     tot_unemp_people = getTotalUnEmployedPeople(result)
     avg_unemp_people = getAverageUnEmployedPeople(result)
     avg_unemp_rate = getAverageUnEmploymentRate(result)
-    
-    return render_template('unemployment_table.html', form=form, result=result, headings=headings, tot_unemp_people=tot_unemp_people, avg_unemp_people=avg_unemp_people, avg_unemp_rate=avg_unemp_rate)
 
-
-@app.route('/county/<get_county>')
-def county(get_county):
-    state_data = counties.query.filter_by(STATEID=get_county).all()
-    countyArray = []
-    for county_info in state_data:
-        countyObj = {}
-        countyObj['STATEID'] = county_info.STATEID
-        countyObj['COUNTYNAME'] = county_info.COUNTYNAME
-        countyArray.append(countyObj)
-    return jsonify({'county_list' : countyArray})
+    return render_template('unemployment_table_county.html', county_list=county_l, result=result, headings=headings, tot_unemp_people=tot_unemp_people, avg_unemp_people=avg_unemp_people, avg_unemp_rate=avg_unemp_rate)
 
 if __name__ == '__main__':
     app.run()
